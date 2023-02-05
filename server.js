3f979a26773d23bbcc4ff87d8ba2247e477330a3
mustafaalberri <114539735+mustafaalberri@users.noreply.github.com>
2023-02-05 23:46:14 +0200

    Updated code, added comments

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// Setup empty JS object to act as endpoint for all routes
+// Setup empty JS object to act as endpoint for all routes.
 projectData = {temp: 0,
                date: '',
                userResponse: ''};
 
 const allData = [];
 
-// Require Express to run server and routes
+// Require Express to run server and routes.
 const express = require('express');
 
-// Start up an instance of app
+// Start up an instance of app.
 const app = express();
 
 /* Middleware*/
@@ -17,28 +17,34 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Cors for cross origin allowance
+// Cors for cross origin allowance.
 const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
+// Initialize the main project folder.
 app.use(express.static('website'));
 
-// Setup Server
+// Setup Server.
 const port = 3000;
 app.listen(port, callbackFn)
+
+// Callback Function for setting up the server.
 function callbackFn(){
     console.log(`Server is up and running with port: ${port}`);
 };
 
+// Set up server side 'GET' request.
 app.get('/getWeatherData', sendData);
 
+// Callback Function for 'GET' request
 function sendData(req, res){
     res.send(projectData);
 }
 
+// Set up server side 'POST' request.
 app.post('/allData', saveData);
 
+// Callback Function for 'POST' request
 function saveData(req, res){
     projectData.temp = req.body.temp;
     projectData.date = req.body.date;
